5d06ab9bec3fbcf1da25dc432ed6d1f75d298783
Bibhas C Debnath <iambibhas@gmail.com>
2012-10-08 22:59:20 +0530

    added github auth classes

---
+++ config-sample.py
@@ -0,0 +1,6 @@
+github_oauth_settings = {
+    'client_id': '',
+    'client_secret': '',
+    'base_url': 'https://github.com/login/oauth/',
+    'redirect_url': 'http://dev.thruflo.com/oauth/callback'
+}
