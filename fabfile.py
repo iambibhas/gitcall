4e00987bb334d6e64e50e62f6f9582d837ad95e0
Bibhas C Debnath <iambibhas@gmail.com>
2012-10-09 13:51:27 +0530

    generalized the fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,31 +5,34 @@ from fabric.contrib import files
 env.user = 'ubuntu'
 # the servers where the commands are executed
 env.hosts = ['23.21.195.1']
+env.project = 'gitcall'
+env.hostname = '%s.bibhas.in' % env.project
 env.key_filename = ["/home/bibhas/.ssh/ec2bi.pem"]
+env.project_path = '/home/ubuntu/sites/gitcall'
 
 def test():
     run('whoami')
 
 def pack():
-    local('git archive HEAD > dist/gitcall.tar')
+    local('git archive HEAD > dist/%s.tar' % env.project)
 
 def upload_tarball():
     # /home/ubuntu/sites/gitcall/
-    put('dist/gitcall.tar', '/home/ubuntu/sites/gitcall/dist/')
+    put('dist/%s.tar' % env.project, '%s/dist/' % env.project_path)
 
 def reload_apache():
     sudo('/etc/init.d/apache2 reload')
 
 def setup_vhost():
-    with cd('/home/ubuntu/sites/gitcall/'):
-        sudo('cp gitcall.vhost /etc/apache2/sites-available/gitcall.bibhas.in')
-        sudo('a2dissite gitcall.bibhas.in')
+    with cd(env.project_path):
+        sudo('cp gitcall.vhost /etc/apache2/sites-available/%s' % env.hostname)
+        sudo('a2dissite %s' % env.hostname)
         reload_apache()
-        sudo('a2ensite gitcall.bibhas.in')
+        sudo('a2ensite %s' % env.hostname)
         reload_apache()
 
 def setup_venv():
-    with cd('/home/ubuntu/sites/gitcall/'):
+    with cd(env.project_path):
         run('virtualenv venv')
         with prefix('source venv/bin/activate'):
             run('pip install -r requirements.txt')
@@ -38,9 +41,9 @@ def deploy():
     pack()
     upload_tarball()
     # Extract files
-    with cd('/home/ubuntu/sites/gitcall/'):
-        run('tar -xvf dist/gitcall.tar')
-    if not files.exists('/home/ubuntu/sites/gitcall/venv/'):
+    with cd(env.project_path):
+        run('tar -xvf dist/%s.tar' % env.project)
+    if not files.exists('%s/venv/' % env.project_path):
         setup_venv()
     # create virtualhost
     setup_vhost()
