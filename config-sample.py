46fa427da0812ed48e9ab4fb378e8c7f53bf47bc
Bibhas C Debnath <iambibhas@gmail.com>
2012-10-15 03:28:25 +0530

    removed unnecessary item

--- config-sample.py
+++ config-sample.py
@@ -2,7 +2,6 @@ github_oauth_settings = {
     'client_id': '',
     'client_secret': '',
     'base_url': 'https://github.com/login/oauth/',
-    'redirect_url': 'http://dev.thruflo.com/oauth/callback'
 }
 plivo_settings = {
     'auth_id': '',
